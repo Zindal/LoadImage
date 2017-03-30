b0f21b1d81f1db6789be43bebf7cf2410a3ed81c
jindal <jindalpatel@live.com>
2017-03-30 12:09:15 +0530

    Initial Commit

---
+++ LoadImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoadImage
+//
+//  Created by Mac User9 on 30/03/17.
+//  Copyright © 2017 Silicon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
